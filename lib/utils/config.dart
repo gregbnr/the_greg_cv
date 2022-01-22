a5cbd742132a3474b9dc2d2430762c56591c6d86
gregoirebonnier <gbonnier@optimetriks.com>
2022-01-22 15:32:58 +0100

    config.dart

---
+++ lib/utils/config.dart
@@ -0,0 +1,14 @@
+///
+/// utils/config.dart
+///
+
+class Config {
+  // https://airtable.com/account/
+  static String airtableApiKey = "keykSp50X8GfqdsjR";
+
+  // https://airtable.com/api
+  static String airtableProjectBase = "appjTjADDw8PPTdNt";
+
+  // database name
+  static String airtableRecordName = "greg_cv";
+}
